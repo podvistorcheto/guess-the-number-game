16c5d5bbbed1ba82502e1965cc6d7a96c6e7b338
podvistorcheto <denev.s.deyan@gmail.com>
2021-08-05 10:07:24 +0000

    Set up game interface

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent =
+    "WOW! IT IS THE CORRECT NUMBER!";
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 20;
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);

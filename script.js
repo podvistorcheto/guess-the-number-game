fa3127a74f527024edf1b0a4e7707a0e99999ac3
podvistorcheto <denev.s.deyan@gmail.com>
2021-08-05 10:20:47 +0000

    game score logic added

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent =
-    "WOW! IT IS THE CORRECT NUMBER!";
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 20;
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);
+// game setup
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent =
+//     "WOW! IT IS THE CORRECT NUMBER!";
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 20;
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+
+// First we need to randomize the numbers to be between 1 and 20 without the decimal signs
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
+// this selects the button check class
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(typeof guess, guess);
+
+    if (!guess) {
+        document.querySelector(".message").textContent =
+            "Invalid input. Please insert a number!";
+    } else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "Bravo! Correct number!";
+    } else if (guess > secretNumber) {
+        document.querySelector(".message").textContent = "Too High";
+        score--;
+        document.querySelector(".score").textContent = score;
+    } else if (guess < secretNumber) {
+        document.querySelector(".message").textContent = "Too Low";
+        score--;
+        document.querySelector(".score").textContent = score;
+    }
+});
